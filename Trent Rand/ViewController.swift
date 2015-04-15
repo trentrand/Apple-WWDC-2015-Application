bbe0d92e3de9bf1b7472c4fd39eba266690ebe26
Trent Rand <Trentmrand@gmail.com>
2015-04-15 02:46:26 -0700

    Initial Commit

---
+++ Trent Rand/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trent Rand
+//
+//  Created by Trent Rand on 4/15/15.
+//  Copyright (c) 2015 Trent Rand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Trent RandTests/Trent_RandTests.swift
@@ -0,0 +1,36 @@
+//
+//  Trent_RandTests.swift
+//  Trent RandTests
+//
+//  Created by Trent Rand on 4/15/15.
+//  Copyright (c) 2015 Trent Rand. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Trent_RandTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
