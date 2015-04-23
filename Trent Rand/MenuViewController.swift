de07fdf6ace58edf249de95e424ea0025ae1a7c1
Trent Rand <Trentmrand@gmail.com>
2015-04-23 01:07:29 -0700

    Added hamburger menu elements

--- Trent Rand/MenuViewController.swift
+++ Trent Rand/MenuViewController.swift
@@ -8,11 +8,66 @@
 
 import UIKit
 
-class MenuViewController: UIViewController {
+class MenuViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet var tableView: UITableView?
+    var tableViewContent: [String] = ["Traits", "Knowledge", "Portfolio", "Contact"]
+    let cellIdentifier = "cellIdentifier"
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.navigationController?.setNavigationBarHidden(false, animated: true)
+        
+        self.tableView!.separatorStyle = UITableViewCellSeparatorStyle.None;
+        self.tableView!.backgroundColor = UIColor.clearColor()
+        self.tableView!.reloadData()
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, indentationLevelForRowAtIndexPath indexPath: NSIndexPath) -> Int {
+        return 0
+    }
+
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 80
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableViewContent.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as! UITableViewCell
+        if (cell.isEqual(nil)) {
+            cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: cellIdentifier)
+        }
+        
+        cell.textLabel!.text = tableViewContent[indexPath.row]
+        
+        var currentFont: UIFont = cell.textLabel!.font
+        var correctFont = UIFont(name: "Avenir-Medium", size: currentFont.pointSize+5)
+        cell.textLabel!.font = correctFont
+        
+        cell.textLabel!.textColor = UIColor.whiteColor()
+        cell.imageView!.image = UIImage(named: tableViewContent[indexPath.row] as String)
+        cell.contentView.backgroundColor = UIColor.clearColor()
+        cell.selectionStyle = UITableViewCellSelectionStyle.None
+        
+        return cell;
+
+    }
+    
+    func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        cell.backgroundColor = UIColor.clearColor()
+    }
+    
+    func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+        // Implement click listener here
+        return nil
     }
     
     override func didReceiveMemoryWarning() {
