03c111f03c073d5c67c79d0cca29891a1b84209a
Trent Rand <Trentmrand@gmail.com>
2015-04-26 17:22:19 -0700

    Updated portfolio view

---
+++ Trent Rand/PortfolioViewController.swift
@@ -0,0 +1,76 @@
+//
+//  PortfolioViewController.swift
+//  Trent Rand
+//
+//  Created by Trent Rand on 4/26/15.
+//  Copyright (c) 2015 Trent Rand. All rights reserved.
+//
+
+import Foundation
+
+class PortfolioViewController: UIViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    
+    var pageController: UIPageViewController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.pageController = UIPageViewController(transitionStyle: UIPageViewControllerTransitionStyle.Scroll, navigationOrientation: UIPageViewControllerNavigationOrientation.Horizontal, options: nil)
+        
+        self.pageController.dataSource = self
+        self.pageController.view.frame = self.view.bounds
+        
+        var initialViewController: PortfolioChildViewController = self.viewControllerAtIndex(0) as! PortfolioChildViewController
+        
+        var viewControllers: Array = Array(arrayLiteral: initialViewController)
+        
+        self.pageController.setViewControllers(viewControllers, direction: UIPageViewControllerNavigationDirection.Forward, animated: false, completion: nil)
+        
+        self.addChildViewController(self.pageController)
+        self.view.addSubview(self.pageController.view)
+        
+        self.pageController.didMoveToParentViewController(self)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        var index: Int = (viewController as! PortfolioChildViewController).index
+        
+        if (index == 0) {
+            return nil
+        }
+        
+        index -= 1
+        return viewControllerAtIndex(index)
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        var index: Int = (viewController as! PortfolioChildViewController).index
+        
+        index++;
+        
+        if (index == 4) {
+            return nil
+        }
+        
+        return viewControllerAtIndex(index)
+    }
+    
+    func viewControllerAtIndex(index: Int) -> UIViewController {
+        var childViewController: PortfolioChildViewController = storyboard?.instantiateViewControllerWithIdentifier("portfolioChildVC") as! PortfolioChildViewController
+        childViewController.index = index
+        
+        return childViewController
+    }
+    
+    func presentationCountForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return 4
+    }
+    
+    func presentationIndexForPageViewController(pageViewController: UIPageViewController) -> Int {
+        return 0
+    }
+}
