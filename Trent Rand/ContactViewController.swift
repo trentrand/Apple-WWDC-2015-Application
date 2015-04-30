97bb2f5014f9867ba92071eea6baa5b5e70d23aa
Trent Rand <Trentmrand@gmail.com>
2015-04-30 04:45:47 -0700

    Update contact view settings

--- Trent Rand/ContactViewController.swift
+++ Trent Rand/ContactViewController.swift
@@ -12,6 +12,28 @@ import MapKit
 class ContactViewController: UIViewController, MKMapViewDelegate {
     @IBOutlet var mapView: MKMapView!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Show
+        let span = MKCoordinateSpanMake(0.1, 0.1)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+        
+        // Drop a pin on my house
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "Trent Rand"
+        annotation.subtitle = "2 miles from Arizona State University"
+        mapView.addAnnotation(annotation)
+        
+        self.createBlur(effectStyle: UIBlurEffectStyle.Light, index: 1)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    
     @IBAction func phoneCall(sender: AnyObject) {
         let phoneNumber = NSURL(string: "tel://6022761234")
             UIApplication.sharedApplication().openURL(phoneNumber!)
@@ -25,23 +47,4 @@ class ContactViewController: UIViewController, MKMapViewDelegate {
         latitude: 33.419241,
         longitude: -111.919582
     )
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Show
-        let span = MKCoordinateSpanMake(0.05, 0.05)
-        let region = MKCoordinateRegion(center: location, span: span)
-        mapView.setRegion(region, animated: true)
-        
-        // Drop a pin on my house
-        let annotation = MKPointAnnotation()
-        annotation.coordinate = location
-        annotation.title = "Trent Rand"
-        annotation.subtitle = "Welcome to my neighborhood"
-        mapView.addAnnotation(annotation)
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
 }
